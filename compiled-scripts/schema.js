3fe06f166921edc42a2d0a54128d08a24da5570a
Ishiah7 <shivern644@gmail.com>
2021-03-25 21:19:12 +0200

    Created the GraphQL server

---
+++ compiled-scripts/schema.js
@@ -0,0 +1,46 @@
+"use strict";
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema, GraphQLID } = require('graphql');
+const axios = require('axios');
+// GraphQL Object Type for people.
+const PeopleType = new GraphQLObjectType({
+    name: 'People',
+    fields: () => ({
+        name: { type: GraphQLString },
+        height: { type: GraphQLString },
+        mass: { type: GraphQLString },
+        hair_color: { type: GraphQLString },
+        skin_color: { type: GraphQLString },
+        eye_color: { type: GraphQLString },
+        birth_year: { type: GraphQLString },
+    })
+});
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        // resolves all people on the API.
+        people: {
+            type: new GraphQLList(PeopleType),
+            resolve() {
+                return axios.get('https://swapi.dev/api/people')
+                    .then((res) => res.data.results);
+            }
+        },
+        // resolves one person depending on the search query.
+        person: {
+            type: PeopleType,
+            args: {
+                height: { type: GraphQLString }
+            },
+            resolve(parent, args) {
+                return axios.get(`https://swapi.dev/api/people/${args.height}`)
+                    .then((res) => res.data);
+            }
+        }
+    }
+});
+// exporting GraphQL Schema
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
+//# sourceMappingURL=schema.js.map
